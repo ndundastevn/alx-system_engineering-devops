4ef80024df322ebb927c2a1a0d3ee5be88d3c483
ndundastevn <ndundastevn@gmail.com>
2023-08-27 05:56:09 -0700

    alx project

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,13 +2,13 @@
 
 include stdlib
 
-file_line { 'Fail to authenticate when password supplied':
+file_line { 'Resuse authentication using passwords':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
   line   => 'PasswordAuthentication no',
 }
 
-file_line { 'Private key for passwordless authentication':
+file_line { 'Only use Private key for passwordless authentication':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
   line   => 'IdentityFile ~/.ssh/school'
